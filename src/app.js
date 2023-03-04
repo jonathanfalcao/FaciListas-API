96f3b6916e634c2694750a53a3103ba1dd719a69
Jonathan Falcão <112404942+jonathanfalcao@users.noreply.github.com>
2023-03-04 11:23:54 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import contentsController from './controllers/contentsController.js'
+
+import express from 'express'
+import cors from 'cors'
+
+// instanciando o servidor
+const app = express()
+
+// configurando o servidor para receber requisições com o corpo no formato JSON
+app.use(express.json())
+
+// configurando o servidor para receber requisições qualquer origem
+app.use(cors())
+
+contentsController.rotas(app)
+
+export default app
---
+++ src/infra/contents.js
@@ -0,0 +1,23 @@
+ /*
+  Esse arquivo deve ser executado apenas uma vez para que a tabela do banco seja criada
+  */
+  import db from "./db.js";
+
+  //==== Conteúdos
+  const CONTENTS_SCHEMA = `
+  CREATE TABLE IF NOT EXISTS "conteudos" (
+      "ID" INTEGER PRIMARY KEY AUTOINCREMENT,
+      "TITULO" varchar(64),
+      "DESCRICAO" varchar(64),
+      "PORCENTAGEM" INTEGER
+    );`;
+
+  function createTableContents() {
+      db.run(CONTENTS_SCHEMA, (error)=> {
+        if (error) console.log("Erro ao criar tabela de conteúdos");
+      });
+  }
+
+  db.serialize( ()=> {
+      createTableContents();
+  });
